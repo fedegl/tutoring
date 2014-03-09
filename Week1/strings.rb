e5ba2fd555d4c40194814ec83b13edc8a0a071ef
Michael Langer <Langermichaellanger@gmail.com>
2014-03-09 14:07:54 -0400

    joe

--- Week1/strings.rb
+++ Week1/strings.rb
@@ -1,3 +1,10 @@
 # Create two strings and then joing them together.
-
+animal = "penguin"
+mammal = "human"
+two_legged_creatures = (animal + mammal)
+address = "98 Montvale Road, Newton, MA"
+address_parts = address.split ","
+#puts address_parts
+puts address_parts[1]
+puts address.length
 # Loop thorugh an array with number from 1 to 10 and print the message "Looping though number: 'number'"
