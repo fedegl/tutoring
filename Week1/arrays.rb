51691046037400ac5d3d4d69b1a44b58b8570a06
Federico Gonzalez <fedegl@gmail.com>
2014-03-10 21:41:22 -0400

    Add array exercises

---
+++ Week1/arrays.rb
@@ -0,0 +1,44 @@
+### Exercises
+#
+
+# 1. Iterate over the sports Array and print out the following list:
+# "0. basketball"
+# "1. baseball"
+# "2. football"
+sports = ["basketball", "baseball", "football"]
+
+
+
+# 2. Create the following array from the existing arrays
+# ["Richard Feynman", "Sam Harris"]
+first_names = ["Richard", "Sam"]
+last_names = ["Feynman", "Harris"]
+
+
+
+# 3. Return true if every element in the array starts with the letter "r"
+tools = ["ruby", "rspec", "rails"]
+
+
+
+# 4. Iterate through the people array and print the following sentences:
+# Lebron is a cool dude
+# Bieber is a jerk face
+people = [["Lebron", "cool dude"], ["Bieber", "jerk face"]]
+
+
+
+# 5. Create an array of all students with test scores greater than 80
+# The result should be [["jon", 99], ["bill", 85]].
+test_scores = [["jon", 99], ["sally", 65], ["bill", 85]]
+
+
+
+# 6. Sort the elements in the array by the number of characters in each word
+words = ["shady", "programming", "bookshelf"]
+
+
+
+# 7. Create a new array with the square of every number in the numbers array
+# The result should be [1,4,9,16,25,36,49,64,81,100]
+numbers = [1,2,3,4,5,6,7,8,9,10]
